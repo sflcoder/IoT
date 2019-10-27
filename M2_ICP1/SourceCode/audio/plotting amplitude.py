7fe1a56286c87aa0b01ee4157312dc3faf90c943
shufeng li <lsfhouse@gmail.com>
2019-10-27 18:03:11 -0500

    Add Audio

---
+++ M2_ICP1/SourceCode/audio/plotting amplitude.py
@@ -0,0 +1,48 @@
+from scipy.io.wavfile import read
+import matplotlib.pyplot as plt
+from os import walk
+import os
+if not os.path.exists("dogPlots"):
+    os.makedirs("dogPlots")
+car_wavs = []
+for (_,_,filenames) in walk('dog'):
+    car_wavs.extend(filenames)
+    break
+for car_wav in car_wavs:
+    # read audio samples
+    input_data = read("dog/" + car_wav)
+    audio = input_data[1]
+    # plot the first 1024 samples
+    plt.plot(audio)
+    # label the axes
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time")
+    # set the title
+    # plt.title("Sample Wav")
+    # display the plot
+    plt.savefig("dogPlots/" + car_wav.split('.')[0] + '.png')
+    # plt.show()
+    plt.close('all')
+
+
+if not os.path.exists("catPlots"):
+    os.makedirs("catPlots")
+car_wavs = []
+for (_,_,filenames) in walk('cat'):
+    car_wavs.extend(filenames)
+    break
+for car_wav in car_wavs:
+    # read audio samples
+    input_data = read("cat/" + car_wav)
+    audio = input_data[1]
+    # plot the first 1024 samples
+    plt.plot(audio)
+    # label the axes
+    plt.ylabel("Amplitude")
+    plt.xlabel("Time")
+    # set the title
+    # plt.title("Sample Wav")
+    # display the plot
+    plt.savefig("catPlots/" + car_wav.split('.')[0] + '.png')
+    # plt.show()
+    plt.close('all')
